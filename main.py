135939808b0d505d9ff41ed9fe4d9b12fbb41fa5
Qaisar Shabbir <qaiser.shabbir55@gmail.com>
2020-02-25 21:05:10 -0600

    Commit 2 Frontier functionality needs to be implemented

--- main.py
+++ main.py
@@ -10,16 +10,10 @@ GAME_SIZE = 3
 class State:
     def __init__(self, config=None, path_cost =0, parent_pointer=None, blank_coordinates=(0,0)):
         self.parent_pointer = parent_pointer
-        self.config = config if config else np.zeros((3,3),'int')
-        self.heuristic = State.calc_heuristic(config)
+        self.config = config if config is not None else np.zeros((3,3),'int')
+        self.heuristic = 0
         self.path_cost = path_cost
         self.blank_coordinates = blank_coordinates
-
-    @staticmethod
-    def calc_heuristic(config):
-        # Calculation
-        # self.heuristic = calculation
-        pass
 
     def update_parent_pointer(self, pointer):
         self.parent_pointer = pointer
@@ -43,6 +37,7 @@ class State:
 
 
 class SearchSolution:
+    goal_coordinates = {0:(0,0), 1:(0,0), 2:(0,0), 3:(0,0), 4:(0,0), 5:(0,0), 6:(0,0), 7:(0,0), 8:(0,0)}
     def __init__(self, start_state=None, goal_state=None, frontier=PriorityQueue(), visited=[], solution_flag=False):
         self.start_state = SearchSolution.take_config_input("start")
         self.current_state = deepcopy(self.start_state)
@@ -50,6 +45,24 @@ class SearchSolution:
         self.frontier = frontier
         self.visited = visited
         self.solution_flag = solution_flag
+
+    def initialize_game(self):
+        SearchSolution.calculate_heuristic(self.start_state)
+        self.goal_state.heuristic = 0
+        self.produce_child()
+
+    @staticmethod
+    def calculate_heuristic(state):
+        distance = 0
+        for i in range(GAME_SIZE-1, -1, -1):
+            for j in range(GAME_SIZE):
+                if state.config[i][j]:
+                    x1, y1 = SearchSolution.goal_coordinates[state.config[i][j]]
+                    x = abs(x1-i)
+                    y = abs(y1-j)
+                    distance += (x+y)
+
+        state.heuristic = distance
 
     @staticmethod
     def take_config_input(input_state):
@@ -59,14 +72,13 @@ class SearchSolution:
             for j in range(GAME_SIZE):
                 while True:
                     try:
-                        val= input(f"Enter value for the tile located at ({i, j}) coordinate position for {input_state} configuration : ")
-                        if (not val and not input_dict[0]):
-                            state.config[i][j] = 0
-                            input_dict[0] = True
-                            state.blank_coordinates = (i, j)
-                            break
-                        elif (1 <= int(val) <= 8 and not input_dict[int(val)]):
-                            state.config[i][j] = int(val)
+                        val= int(input(f"Enter value for the tile located at ({i, j}) coordinate position for {input_state} configuration : "))
+                        if (0 <= val <= 8 and not input_dict[int(val)]):
+                            state.config[i][j] = val
+                            if input_state == "end":
+                                SearchSolution.goal_coordinates[val] = (i, j)
+                            if not val:
+                                state.blank_coordinates = (i, j)
                             input_dict[int(val)] = True
                             break
                         else:
@@ -80,10 +92,10 @@ class SearchSolution:
     def produce_child(self):
         i, j = self.current_state.blank_coordinates
         children = []
-        if i > 0:
-            children.append((i-1, j))       # Up
         if i < GAME_SIZE-1:
-            children.append((i+1, j))       # Down
+            children.append((i+1, j))       # Up
+        if i > 0 :
+            children.append((i-1, j))       # Down
         if j > 0:
             children.append((i,j-1))        #Left
         if j < GAME_SIZE-1:
@@ -91,14 +103,16 @@ class SearchSolution:
 
 
         for child in children:
-            state = self.produce_state(child, self.current_state)
-            self.frontier.put(state)    # If it does not exist in the frontier already other wise update cost
+            state = self.produce_state(child, deepcopy(self.current_state))
+            SearchSolution.calculate_heuristic(state)
+            fn = state.heuristic + state.path_cost
+            self.frontier.put((fn, state))    # If it does not exist in the frontier already other wise update cost
 
     def produce_state(self, coordinates , state):
         x1, y1 = state.blank_coordinates
         x2, y2 = coordinates
         config = deepcopy(state.config)
-        config[x1, y1] , config[x2, y2] = config[x2,y2], config[x1, y1]
+        config[x1, y1], config[x2, y2] = config[x2, y2], config[x1, y1]
         path_cost = state.path_cost + ACTION_COST
         blank_coordinates = coordinates
         parent_pointer = state
@@ -117,10 +131,13 @@ class SearchSolution:
 
 def main():
     game = SearchSolution()
-    game.current_state.config[0][0] = 9
+    # game.current_state.config[0][0] = 9
     game.start_state.print_state()
     game.goal_state.print_state()
     game.current_state.print_state()
+    print(game.start_state.blank_coordinates)
+    game.initialize_game()
+    print(game.start_state.heuristic)
 
 main()
 
